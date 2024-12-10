e49d4e8596457e9d1bbfe246aa8abef861b563ce
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-09 22:13:17 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,20 @@
-function binarySearch(list, element) {
+function binarySearch(list, element) 
+{
     let left = 0;
     let right = list.length - 1;
 
-    while (left <= right) {
+    while (left <= right) 
+    {
         let mid = Math.floor((left + right) / 2);
 
-        if (list[mid] === element) {
+        if (list[mid] === element)
+        {
             return mid; 
-        } else if (list[mid] < element) {
+        } else if (list[mid] < element) 
+        {
             left = mid + 1
-        } else {
+        } else 
+        {
             right = mid - 1;
         }
     }
